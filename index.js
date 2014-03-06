f461a85e50abb2f6270106ba7ab67946acda4143
Oleg Slobodskoi <oleg008@gmail.com>
2014-03-06 16:24:38 +0100

    first release

---
+++ index.js
@@ -0,0 +1,82 @@
+(function(window) {
+'use strict'
+
+/**
+ * Modules registry.
+ *
+ * @type {Object}
+ * @api public
+ */
+var modules = define.modules = {}
+
+/**
+ * Define a module.
+ *
+ * @param {String} [id] - file path or any identifier
+ * @param {Array} [deps] - optional dependencies for Commonjs conformance only
+ * @param {Function} factory
+ * @api public
+ */
+function define(id, deps, factory) {
+    var module
+
+    // define(function() {})
+    if (typeof id == 'function') {
+        factory = id
+        id = null
+    // define('id', function() {})
+    } else if (typeof deps == 'function') {
+        factory = deps
+        deps = null
+    }
+
+    if (typeof factory != 'function') throw new Error('Module factory required.')
+
+    module = {factory: factory, id: id}
+
+    if (id) {
+        modules[id] = module
+    } else {
+        module.exports = {}
+        define.exec(module)
+    }
+}
+
+window.define = define
+
+/**
+ * Execute the module factory.
+ *
+ * @param {Object} module
+ * @api private
+ */
+define.exec = function(module) {
+    return module.factory.call(module.exports, require, module.exports, module)
+}
+
+/**
+ * Get the dependency. Executes factory if not already done.
+ *
+ * @param {String} id
+ * @return {Mixed} module.exports
+ * @api public
+ */
+function require(id) {
+    var module = modules[id],
+        newExports
+
+    if (!module) throw new Error('Module ' + id + ' not found.')
+
+    if (!module.exports) {
+        module.exports = {}
+        newExports = define.exec(module)
+        if (newExports) module.exports = newExports
+    }
+
+    return module.exports
+}
+
+window.require = require
+
+}(window))
+
---
+++ readme.md
@@ -0,0 +1,46 @@
+## Micro implementation of commonjs modules
+
+This implementation will work with ANY existing commonjs modules where almond or requirejs would work. This module is created because I am not using the most features of requirejs and because I can live without the most features of almond.
+
+But the main reason is - control over execution order of the modules.
+
+- Absolut minimalistic, few lines of code.
+- Define your module.
+- Require your module.
+- Module factory executed only when required.
+- You control the execution order by requiring modules!
+- Define aliases.
+
+## Outside of this modules scope
+- Relative path resolution. (Ids are format agnostic strings.)
+- Remote loading
+- Anything else you know from requirejs
+
+## Api
+
+### `define([id], factory)`
+
+Define your dependency. Factory is executed when required, `this` points to exports.
+
+    define('mymodule', function(require, exports, module) {
+        module.id == 'mymodule' // true
+        this === exports // true
+        exports === module.exports // true
+        require === window.require // true
+        exports.test = '123'
+    })
+
+### `require(id)`
+
+Require your dependency. Id is a format agnostic string.
+
+    require('mymodule').test == '123' // true
+
+### Define aliases
+
+    define.modules.mymodule2 = define.modules.mymodule
+
+## License
+
+MIT
+
